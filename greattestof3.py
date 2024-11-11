8fbccc53c73fc784abc81f06f309065790f5b1bf
ahmadrv <ahmadmahmoodiandarvishani@gmail.com>
2024-11-11 22:53:17 +0330

    add a example to test usage of python_graphs

---
+++ greattestof3.py
@@ -0,0 +1,16 @@
+from python_graphs import control_flow, control_flow_graphviz
+
+def greattestof3(x, y, z):
+    if ((x > y) and (x > z)):
+        return x
+    elif ((y > z) and (y > x)):
+        return y
+    else:
+        return z
+
+
+graph = control_flow.get_control_flow_graph(greattestof3)
+agraph = control_flow_graphviz.to_graphviz(graph)
+
+agraph.layout(prog="dot")
+agraph.draw("output.svg", format="svg")
